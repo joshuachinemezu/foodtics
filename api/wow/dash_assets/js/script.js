2c5de9717304e79507c6b5287d7c791605a32092
Joshua <joshuachinemezu@gmail.com>
2018-07-21 11:29:57 +0100

    Remove duplicated directory

--- api/wow/dash_assets/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-            $(window).scroll(function() { 
-                if ($(document).scrollTop() > 50) { 
-                    $(".navbar-fixed-top").css("background-color", "#2dc997"); 
-                } else {
-                $(".navbar-fixed-top").css("background-color", "#ffffff");
-            }
-            });
-        });
